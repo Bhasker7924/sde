fba3e81523abe0b63e493377106c7d95e8ade778
Bhasker Reddy <106075408+Bhasker7924@users.noreply.github.com>
2025-07-21 22:25:28 +0530

    Update FormContext.tsx

--- app/components/FormContext.tsx
+++ app/components/FormContext.tsx
@@ -3,7 +3,7 @@
 
 import React, { createContext, useContext, useState, ReactNode } from 'react';
 
-type FormData = {
+export type FormData = {
   name: string;
   email: string;
   linkedin: string;
